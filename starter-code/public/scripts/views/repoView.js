bcd771c0c4c41ae5762b66608e47962ad676dae8
derekcfarmer <derek.farmer@gmail.com>
2017-05-23 14:06:22 -0700

    added call and render function, requestRepos error

--- starter-code/public/scripts/views/repoView.js
+++ starter-code/public/scripts/views/repoView.js
@@ -12,8 +12,10 @@ var app = app || {};
     $about.show().siblings().hide();
   };
 
-  // TODO: Remember that new Handlebars template? Let's compile it!
+  // TODO - DONE: Remember that new Handlebars template? Let's compile it!
   // Save the result in this `render` variable.
+
+  let render = Handlebars.compile($('#repo-template').text());
 
   repoView.index = function() {
     ui();
